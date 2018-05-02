e638cac6bde5889eea6169db3ddd830a74ad1663
Kevin Wu <kevin@kevinniuwu.com>
2018-05-01 21:22:59 -0400

    Prettify output

--- websocket_handler.py
+++ websocket_handler.py
@@ -1,11 +1,12 @@
 import json
 import logging
 
-from colorama import Fore, Style
 from lomond import WebSocket
 from unidecode import unidecode
 
 import settings
+from question_handler import QuestionHandler
+from tools import color, colors
 
 
 class WebSocketHandler:
@@ -14,6 +15,7 @@ class WebSocketHandler:
         self.logging_enabled = settings.get("LOGGING", "Enable")
         self.log_chat = settings.get("LOGGING", "LogChat")
         self.show_question_summary = settings.get("LIVE", "ShowQuestionSummary")
+        self.question_handler = QuestionHandler()
 
     def connect(self, uri):
         websocket = WebSocket(uri)
@@ -24,7 +26,8 @@ class WebSocketHandler:
             if event.name == "text":
                 message = json.loads(event.text)
 
-                if message["type"] != "interaction" or (self.log_chat and message["type"] == "interaction"):
+                if self.logging_enabled and \
+                        (message["type"] != "interaction" or (self.log_chat and message["type"] == "interaction")):
                     logging.debug(message)
 
                 if "error" in message and message["error"] == "Auth not valid":
@@ -34,17 +37,19 @@ class WebSocketHandler:
                     choices = [unidecode(choice["text"]) for choice in message["answers"]]
 
                     print("\n"*5)
-                    print("Question detected.")
                     print(f"Question {message['questionNumber']} out of {message['questionCount']}")
-                    print(f"{Fore.CYAN}{question}\nChoices: {', '.join(choices)}{Style.RESET_ALL}\n")
+                    print(color(question, colors.BLUE))
+                    print("Choices:", color(", ".join(choices), colors.BLUE))
+
+                    self.question_handler.answer_question(question, choices)
                 elif self.show_question_summary and message["type"] == "questionSummary":
                     question = unidecode(message["question"])
-                    print(f"\nQuestion Summary: {question}")
+                    print(f"\nQuestion summary: {color(question, colors.BLUE)}")
 
                     for answer in message["answerCounts"]:
                         ans_str = unidecode(answer["answer"])
-                        color = Fore.GREEN if bool(answer["correct"]) else Fore.RED
-                        print(f"{color}{ans_str}:{answer['count']}:{answer['correct']}{Style.RESET_ALL}")
+                        print_color = colors.GREEN if bool(answer["correct"]) else colors.RED
+                        print(color(f"{ans_str}:{answer['count']}:{answer['correct']}", print_color))
 
                     print(f"{message['advancingPlayersCount']} players advancing")
                     print(f"{message['eliminatedPlayersCount']} players eliminated\n")
