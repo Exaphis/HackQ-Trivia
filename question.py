d4bc94a83cb9bb0be16bf9bf130d8a90b095bc19
Exaphis <kevin@kevinniuwu.com>
2018-06-06 13:09:34 -0400

    Use Aho-Corasick algorithm for faster search

--- question.py
+++ question.py
@@ -1,6 +1,13 @@
+import ahocorasick
 import logging
 from time import time
 
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.tag.perceptron import PerceptronTagger
+from nltk.tokenize import RegexpTokenizer
+
+from settings import config
 from tools import init_logger, punctuation_to_space, punctuation_to_none
 
 
@@ -9,6 +16,14 @@ class QuestionHandler:
         if "HackQ" not in logging.Logger.manager.loggerDict:
             init_logger()
         self.logger = logging.getLogger("HackQ")
+
+        self.simplified_output = config.getboolean("LIVE", "SimplifiedOutput")
+
+        self.fsa = ahocorasick.Automaton()
+
+        self.STOPWORDS = set(stopwords.words("english")) - {"most", "least"}
+        self.nltk_tokenizer = RegexpTokenizer(r"\w+")
+        self.nltk_tagger = PerceptronTagger()
 
     def answer_question(self, question, original_choices):
         print("Searching...")
@@ -26,14 +41,66 @@ class QuestionHandler:
 
         print(f"Search took {round(time()-start_time, 2)} seconds")
 
-    def __method1(self):
-        pass
+    def __method1(self, texts, answers, reverse):
+        print("Running method 1")
+
+        counts = {}
+        for idx, a in enumerate(answers):
+            answer = a.lower()
+            counts[answer] = 0
+            self.fsa.add_word(f" {answer} ", idx)
+
+        self.fsa.make_automaton()
+
+        for text in texts:
+            for _, (_, match) in self.fsa.iter(text):
+                counts[match] += 1
+        self.fsa.clear()
+
+        print(counts)
+        # If not all answers have count of 0 and the best value doesn't occur more than once, return the best answer
+        best_value = min(counts.values()) if reverse else max(counts.values())
+        if not all(c == 0 for c in counts.values()) and list(counts.values()).count(best_value) == 1:
+            print(counts)
+            return min(counts, key=counts.get) if reverse else max(counts, key=counts.get)
+
+        return ""
 
     def __method2(self):
-        pass
+        print("Running method 2")
 
     def __method3(self):
         pass
+
+    def find_keywords(self, words):
+        return [w for w in self.nltk_tokenizer.tokenize(words.lower()) if w not in self.STOPWORDS]
+
+    def find_nouns(self, text, num_words, reverse=False):
+        tokens = word_tokenize(text)
+        tags = [tag for tag in self.nltk_tagger.tag(tokens) if tag[1] != "POS"]
+
+        if not self.simplified_output:
+            print(tags)
+
+        tags = tags[:num_words] if not reverse else tags[-num_words:]
+
+        nouns = []
+        consecutive_nouns = []
+
+        for tag in tags:
+            tag_type = tag[1]
+            word = tag[0]
+
+            if "NN" not in tag_type and len(consecutive_nouns) > 0:
+                nouns.append(" ".join(consecutive_nouns))
+                consecutive_nouns = []
+            elif "NN" in tag_type:
+                consecutive_nouns.append(word)
+
+        if len(consecutive_nouns) > 0:
+            nouns.append(" ".join(consecutive_nouns))
+
+        return nouns
 
     @staticmethod
     def get_best_answer(all_scores, choice_groups, reverse=False):
