72313da46c6e94361d7d66568d534796fd64aadc
Exaphis <kevin@kevinniuwu.com>
2020-06-25 18:35:16 -0700

    Allow search unittests to test Google and Bing concurrently

--- hackq_trivia/searcher.py
+++ hackq_trivia/searcher.py
@@ -24,14 +24,16 @@ class Searcher:
         self.timeout = config.getfloat("CONNECTION", "Timeout")
         self.search_service = config.get("SEARCH", "Service")
 
+        bing_api_key = config.get("SEARCH", "BingApiKey")
+        self.bing_headers = {"Ocp-Apim-Subscription-Key": bing_api_key}
+
+        self.google_cse_id = config.get("SEARCH", "GoogleCseId")
+        google_api_key = config.get("SEARCH", "GoogleApiKey")
+        self.google_service = googleapiclient.discovery.build("customsearch", "v1", developerKey=google_api_key)
+
         if self.search_service == "Bing":
-            api_key = config.get("SEARCH", "BingApiKey")
-            self.search_headers = {"Ocp-Apim-Subscription-Key": api_key}
             self.search_func = self.get_bing_links
         elif self.search_service == "Google":
-            self.cse_id = config.get("SEARCH", "GoogleCseId")
-            api_key = config.get("SEARCH", "GoogleApiKey")
-            self.google_service = googleapiclient.discovery.build("customsearch", "v1", developerKey=api_key)
             self.search_func = self.get_google_links
         else:
             raise InvalidSearchServiceError(f"Search service type {self.search_service} was not recognized.")
@@ -60,13 +62,13 @@ class Searcher:
         self.search_func(query, num_results)
 
     def get_google_links(self, query, num_results):
-        response = self.google_service.cse().list(q=query, cx=self.cse_id, num=num_results).execute()
+        response = self.google_service.cse().list(q=query, cx=self.google_cse_id, num=num_results).execute()
         return list(map(operator.itemgetter("link"), response["items"]))
 
     def get_bing_links(self, query, num_results):
         # could be using aiohttp here...
         search_params = {"q": query, "count": num_results}
-        resp = requests.get(self.BING_ENDPOINT, headers=self.search_headers, params=search_params)
+        resp = requests.get(self.BING_ENDPOINT, headers=self.bing_headers, params=search_params)
 
         if resp.status_code != requests.codes.ok:
             logging.error(f"Bing search failed with status code {resp.status_code}")
